822c50786a8226d384037720217dc067342877a4
victoricoma <victorsena3010@gmail.com>
2026-05-20 12:28:50 -0300

    Lista de Imagens Upload Aula0520

--- backend/helpers/image-upload.js
+++ backend/helpers/image-upload.js
@@ -1,32 +1,32 @@
-const multer = require('multer')
-const path = require('path')
+const multer = require("multer");
+const path = require("path");
 
 const imageStorage = multer.diskStorage({
     destination: function (req, file, cb) {
-        let folder = ''
+        let folder = "";
+
+        console.log(req)
 
         if (req.baseUrl.includes('users')) {
-            folder = 'users'
+            folder = "users";
         } else if (req.baseUrl.includes('pets')) {
-            folder = 'pets'
+            folder = "pets";
         }
-
-        cb(null, `public/images/${folder}`)
+        cb(null, `public/images/${folder}/`);
     },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname))
-    }
-
-})
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
 
 const imageUpload = multer({
     storage: imageStorage,
     fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
-            return cb(new Error('Por favor, envie apenas arquivos jpg ou png.'))
+        if (!file.originalname.match(/\.(png|jpg)$/)) {
+            return cb(new Error("Por favor, envie apenas png ou jpg!"));
         }
-        cb(undefined, true)
-    }
-})
+        cb(undefined, true);
+    },
+});
 
-module.exports = { imageUpload }
+module.exports = { imageUpload };
