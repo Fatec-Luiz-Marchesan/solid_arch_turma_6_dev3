fc9ba8838161d62e32e40e27bc9db2cad6ac052f
daniykt <daniykt7@gmail.com>
2026-06-17 09:38:27 -0300

    Refatorar: Remover código desnecessário e simplificar a inicialização do servidor com MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,102 +1,16 @@
-const express = require("express");
-const cors = require("cors");
+const mongoose = require("mongoose");
+const app = require("./src/app");
 
-const app = express();
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/solid_arch";
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-app.use(express.static("public"));
-app.use(makeHealthRouter());
-
-const PetRoutes = require("./routers/PetRouters");
-const UserRoutes = require("./routers/UserRouters");
-const ReviewRoutes = require("./routers/ReviewRouters");
-const makeHealthRouter = require('./routers/healthRoutes');
-const reviewRepository = new ReviewMongoRepository();
-const createReviewUseCase = new CreateReviewUseCase({ reviewRepository });
-const listReviewsUseCase = new ListReviewsUseCase({ reviewRepository });
-const getReviewUseCase = new GetReviewUseCase({ reviewRepository });
-const deleteReviewUseCase = new DeleteReviewUseCase({ reviewRepository });
-
-const reviewController = new ReviewController({
-  createReviewUseCase,
-  listReviewsUseCase,
-  getReviewUseCase,
-  deleteReviewUseCase,
-});
-
-const {
-  getRedisClient,
-  closeRedisClient,
-} = require("./infrastructure/adapters/cache/redisClient");
-const RedisCacheAdapter = require("./infrastructure/adapters/cache/RedisCacheAdapter");
-const ListAdminsUseCase = require("./domain/usecases/ListAdminsUseCase");
-const redisClient = getRedisClient();
-const cacheGateway = new RedisCacheAdapter(redisClient);
-const listAdminsUseCase = new ListAdminsUseCase({
-  adminRepository,
-  cacheGateway,
-});
-
-const DietMongoRepository = require("./infrastructure/repositories/DietMongoRepository");
-const {
-  CreateDietUseCase,
-  GetActiveDietForPetUseCase,
-  ListDietsUseCase,
-  UpdateDietUseCase,
-} = require("./domain/usecases/CreateDietUseCase");
-const DietController = require("./interface/controllers/DietController");
-const makeDietRouter = require("./interface/routes/dietRoutes");
-
-const dietRepository = new DietMongoRepository();
-const dietController = new DietController({
-  createDietUseCase: new CreateDietUseCase({ dietRepository }),
-  listDietsUseCase: new ListDietsUseCase({ dietRepository }),
-  getActiveDietForPetUseCase: new GetActiveDietForPetUseCase({
-    dietRepository,
-  }),
-  updateDietUseCase: new UpdateDietUseCase({ dietRepository }),
-});
-
-const UserMongoRepository = require("./src/infrastructure/repositories/UserMongoRepository");
-const BcryptHashAdapter = require("./src/infrastructure/adapters/auth/BcryptHashAdapter");
-const CreateUserUseCase = require("./src/domain/usecases/CreateUserUseCase");
-const GetUserUseCase = require("./src/domain/usecases/GetUserUseCase");
-const UpdateUserUseCase = require("./src/domain/usecases/UpdateUserUseCase");
-const DeleteUserUseCase = require("./src/domain/usecases/DeleteUserUseCase");
-const NewUserController = require("./src/interface/controllers/UserController");
-const makeUserRouter = require("./src/interface/routes/userRoutes");
-
-const userRepository = new UserMongoRepository();
-const hashGateway = new BcryptHashAdapter();
-const newUserController = new NewUserController({
-  createUserUseCase: new CreateUserUseCase({ userRepository, hashGateway }),
-  getUserUseCase: new GetUserUseCase({ userRepository }),
-  updateUserUseCase: new UpdateUserUseCase({ userRepository, hashGateway }),
-  deleteUserUseCase: new DeleteUserUseCase({ userRepository }),
-});
-
-const {
-  getESClient,
-} = require("./infrastructure/adapters/search/elasticsearchClient");
-const ElasticSearchAdapter = require("./infrastructure/adapters/search/ElasticSearchAdapter");
-const SearchPetsUseCase = require("./domain/usecases/SearchPetsUseCase");
-
-const esClient = getESClient();
-const searchGateway = new ElasticSearchAdapter(esClient);
-const searchPetsUseCase = new SearchPetsUseCase({ searchGateway });
-
-const PetController = require('./controllers/PetController');
-PetController.deps = { searchPetsUseCase, indexPetUseCase };
-
-app.get("/api/pets/search", (req, res) => petController.search(req, res));
-
-app.use("/pets", PetRoutes);
-app.use("/users", UserRoutes);
-app.use("/reviews", ReviewRoutes);
-app.use("/admins", makeAdminRouter(listAdminsUseCase));
-app.use("/api/diets", makeDietRouter(dietController));
-app.use("/api/users", makeUserRouter(newUserController));
-app.use("/uploads", express.static("uploads"));
-
-app.listen(5000);
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("MongoDB conectado");
+    app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+  })
+  .catch((err) => {
+    console.error("Erro ao conectar MongoDB:", err);
+    process.exit(1);
+  });
